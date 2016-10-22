00797ea8b6c2e71b3aa67b0b270a63f17c56a8d0
jackyhsuch <jackyhsu0102@gmail.com>
2016-10-22 17:27:35 +0800

    change contact from char to int

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Users(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    contact = models.CharField(max_length=200)
+    contact = models.IntegerField()
     matric_number = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
