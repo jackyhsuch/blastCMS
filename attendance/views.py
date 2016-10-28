4b6ec03a6e29e9a022a9894a1584e1fdbbd921e8
jackyhsuch <jackyhsu0102@gmail.com>
2016-10-28 15:08:41 +0800

    add event

--- attendance/views.py
+++ attendance/views.py
@@ -1,11 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from users.models import Users
+from .models import Events
+from .models import Attendances
+from datetime import datetime
 
+# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    event_list = Events.objects.order_by('id')
+    context = {
+        'event_list': event_list,
+    }
+    return render(request, 'attendance/index.html', context)
 
 
 def new(request):
-        context = {}
-        return render(request, 'attendance/new.html', context)
+    context = {}
+
+    if request.method == 'POST':
+        # print(request.POST)
+        name = request.POST['name']
+        date = request.POST['date']
+        years = request.POST.getlist('years')
+
+        # create event
+        newEvent = Events(
+            name=name,
+            date=date,
+            isCompulsory=True
+        )
+        newEvent.save()
+
+        eventId = newEvent.id
+
+
+        # get all the users
+        usersList = []
+        for year in years:
+            batchYear = datetime.now().year - int(year)
+            users = list(Users.objects.filter(batch_year=batchYear))
+            usersList += users
+
+
+        # populate to attendance
+        for user in usersList:
+            print(user.name)
+            print(user.batch_year)
+            newRow = Attendances(
+                event_id=eventId,
+                user_id=user.id
+            )
+
+            newRow.save()
+
+    return render(request, 'attendance/new.html', context)
+
+
+# show each event details with name list
+def show(request, event_id):
+    event = get_object_or_404(Events, pk=event_id)
+    context = {'event': event}
+
+    return render(request, 'attendance/show.html', context)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from datetime import datetime
+
 
 # Create your models here.
 class Users(models.Model):
@@ -7,16 +9,14 @@ class Users(models.Model):
     email = models.CharField(max_length=200)
     contact = models.IntegerField()
     matric_number = models.CharField(max_length=200)
+    batch_year = models.IntegerField(default=datetime.now().year)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-
     def __str__(self):
         sb = []
-        
+
         for key in self.__dict__:
             sb.append("{key}='{value}'".format(key=key, value=self.__dict__[key]))
- 
+
         return ', '.join(sb)
-        
